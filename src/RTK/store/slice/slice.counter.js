58c327bd5d86557253aa24abeb41745a638cea82
bhuwanjungthapa <bhuwanjung115@gmail.com>
2023-02-02 16:43:36 +0545

    Thunk Function

--- src/RTK/store/slice/slice.counter.js
+++ src/RTK/store/slice/slice.counter.js
@@ -1,4 +1,5 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { decrementByOne, incrementByOne } from "../../../asyncFunc";
 
 const initialState = {value: 0}
 const counterSlice = createSlice({
@@ -13,7 +14,22 @@ const counterSlice = createSlice({
             state.value -= 1;
         },
     }
-})
+});
+export const asyncIncrementByOne = createAsyncThunk(
+    'counter/incrementByOne',
+    async () => {
+        const respose = await incrementByOne();
+        return {value: respose};
+    }
+)
+
+// export const asyncDecrementtByOne = createAsyncThunk(
+//     'counter/decrementByOne',
+//     async () => {
+//         const respose = await decrementByOne();
+//         return {value: respose};
+//     }
+// )
 
 export const{ INCREMENT, DECREMENT} = counterSlice.actions;
 
